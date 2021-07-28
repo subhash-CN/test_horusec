4f0dce4626c00488592fd4beea745a992dae65fb
subhash-CN <85877879+subhash-CN@users.noreply.github.com>
2021-07-28 13:34:58 +0530

    Update App.java

--- App.java
+++ App.java
@@ -5,6 +5,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        String s = "" + 123;                // inefficient
+String t = Integer.toString(456);   // preferred approach
+        
         Random rand = new Random();
         System.out.println(rand.nextInt(50));
         System.out.println( "Hello World!" );
